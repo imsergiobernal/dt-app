648170dd392d01f10f3b1fb9c2933998ccf0aac1
Sergio Bernal Guillot <sergioguillot@gmail.com>
2018-11-18 16:14:00 +0100

    Static dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -13,18 +13,26 @@ export class DashboardComponent {
     map(({ matches }) => {
       if (matches) {
         return [
-          { title: 'Card 1', cols: 1, rows: 1 },
-          { title: 'Card 2', cols: 1, rows: 1 },
-          { title: 'Card 3', cols: 1, rows: 1 },
-          { title: 'Card 4', cols: 1, rows: 1 }
+          { title: 'Card 1', image: 'https://material.angular.io/assets/img/examples/shiba2.jpg', body: 'Lorem Ipsum', publisher: 'elpais.com', source: 'https://elpais.com/t', cols: 4, rows: 1 },
+          { title: 'Card 2', image: 'https://material.angular.io/assets/img/examples/shiba2.jpg', body: 'Lorem Ipsum', publisher: 'elpais.com', source: 'https://elpais.com/t', cols: 4, rows: 1 },
+          { title: 'Card 3', image: 'https://material.angular.io/assets/img/examples/shiba2.jpg', body: 'Lorem Ipsum', publisher: 'elpais.com', source: 'https://elpais.com/t', cols: 4, rows: 1 },
+          { title: 'Card 4', image: 'https://material.angular.io/assets/img/examples/shiba2.jpg', body: 'Lorem Ipsum', publisher: 'elpais.com', source: 'https://elpais.com/t', cols: 4, rows: 1 },
+          { title: 'Card 1', image: 'https://material.angular.io/assets/img/examples/shiba2.jpg', body: 'Lorem Ipsum', publisher: 'elpais.com', source: 'https://elpais.com/t', cols: 4, rows: 1 },
+          { title: 'Card 2', image: 'https://material.angular.io/assets/img/examples/shiba2.jpg', body: 'Lorem Ipsum', publisher: 'elpais.com', source: 'https://elpais.com/t', cols: 4, rows: 1 },
+          { title: 'Card 3', image: 'https://material.angular.io/assets/img/examples/shiba2.jpg', body: 'Lorem Ipsum', publisher: 'elpais.com', source: 'https://elpais.com/t', cols: 4, rows: 1 },
+          { title: 'Card 4', image: 'https://material.angular.io/assets/img/examples/shiba2.jpg', body: 'Lorem Ipsum', publisher: 'elpais.com', source: 'https://elpais.com/t', cols: 4, rows: 1 }
         ];
       }
 
       return [
-        { title: 'Card 1', cols: 2, rows: 1 },
-        { title: 'Card 2', cols: 1, rows: 1 },
-        { title: 'Card 3', cols: 1, rows: 2 },
-        { title: 'Card 4', cols: 1, rows: 1 }
+        { title: 'Card 1', image: 'https://material.angular.io/assets/img/examples/shiba2.jpg', body: 'Lorem Ipsum', publisher: 'elpais.com', source: 'https://elpais.com/t', cols: 1, rows: 1 },
+        { title: 'Card 2', image: 'https://material.angular.io/assets/img/examples/shiba2.jpg', body: 'Lorem Ipsum', publisher: 'elpais.com', source: 'https://elpais.com/t', cols: 1, rows: 1 },
+        { title: 'Card 3', image: 'https://material.angular.io/assets/img/examples/shiba2.jpg', body: 'Lorem Ipsum', publisher: 'elpais.com', source: 'https://elpais.com/t', cols: 1, rows: 1 },
+        { title: 'Card 4', image: 'https://material.angular.io/assets/img/examples/shiba2.jpg', body: 'Lorem Ipsum', publisher: 'elpais.com', source: 'https://elpais.com/t', cols: 1, rows: 1 },
+        { title: 'Card 1', image: 'https://material.angular.io/assets/img/examples/shiba2.jpg', body: 'Lorem Ipsum', publisher: 'elpais.com', source: 'https://elpais.com/t', cols: 1, rows: 1 },
+        { title: 'Card 2', image: 'https://material.angular.io/assets/img/examples/shiba2.jpg', body: 'Lorem Ipsum', publisher: 'elpais.com', source: 'https://elpais.com/t', cols: 1, rows: 1 },
+        { title: 'Card 3', image: 'https://material.angular.io/assets/img/examples/shiba2.jpg', body: 'Lorem Ipsum', publisher: 'elpais.com', source: 'https://elpais.com/t', cols: 1, rows: 1 },
+        { title: 'Card 4', image: 'https://material.angular.io/assets/img/examples/shiba2.jpg', body: 'Lorem Ipsum', publisher: 'elpais.com', source: 'https://elpais.com/t', cols: 1, rows: 1 }
       ];
     })
   );
